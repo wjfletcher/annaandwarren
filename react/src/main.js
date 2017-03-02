572cfd349ae738654b7fcf2e403d69a23707115e
wjfletcher <warrenjfletcher@gmail.com>
2017-03-02 15:38:08 -0500

    yml file and react router

--- react/src/main.js
+++ react/src/main.js
@@ -2,10 +2,14 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { browserHistory } from 'react-router';
+
+import routes from './routes';
+import Root from './components/Root';
 
 $(function() {
   ReactDOM.render(
-    <App/>,
+    <Root browserHistory={browserHistory} routes={routes} />,
     document.getElementById('app')
   );
 });
