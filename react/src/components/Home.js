c2facef2e0182c88179dd1958b379ffd0745c5e1
wjfletcher <warrenjfletcher@gmail.com>
2017-03-08 18:08:13 -0500

    add RSVP and requests

--- react/src/components/Home.js
+++ react/src/components/Home.js
@@ -33,9 +33,9 @@ class Home extends Component {
                   <ul className="nav navbar-nav navbar-left">
                       <li><a href="#jumbotron">Home</a>
                       </li>
-                      <li><a href="#content1">Date and Location</a>
+                      <li><a href="#content1">RSVP</a>
                       </li>
-                      <li><a href="#content2">Food</a>
+                      <li><a href="#content2">Song/Beer Requests</a>
                       </li>
 
                   </ul>
@@ -48,63 +48,59 @@ class Home extends Component {
       <div className="jumbotron center" id="jumbotron">
           <div className="spacer"></div>
           <div className="title">
-              <h1>Anna and Warren</h1>
-              <p>Were getting married!</p>
-
-
-
+            <h1>Anna and Warren</h1>
+            <p>are getting married!</p>
+            <h1>September 16th, 2017</h1>
           </div>
 
       </div>
-      <div className="forestdiv"><img className="forest" src="/assets/forest.png"/></div>
+
 
       <div id="content1">
-          <div id="firstsec">
-              <div className="container">
-
-                  <br/>
-                  <br/>
-                  <div className="center">
-                      <h1>Date</h1>
-                      <br/>
-                      <h3>September 16th, 2017</h3>
-                  </div>
-                  <br/>
-                  <br/>
-
-
-              </div>
-              <br/>
-              <br/>
-              <br/>
+        <div id="firstsec">
+          <div className="container">
+            <div className="center">
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfNCaEm7s8yShV_KnLP0JmRwA_Ddmk3w_0GGdGgUFSysR0yrw/viewform?embedded=true" width="100%" height="830" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+            </div>
           </div>
-          <div id="secsec">
-              <div className="container">
-              <div id="map-canvas" className="col-md-6"></div>
-              <div className="col-md-4 hidden-xs center" id="location">
+        </div>
+        <div id="secsec">
+          <div className="container">
+            <div id="map-canvas" className="col-md-6"></div>
+            <div className="col-md-4 hidden-xs center" id="location">
 
-                  <h2>The Venue</h2>
-                  <p>Southport Island, Maine</p>
+                <h2>The Venue</h2>
+                <p>Southport Island, Maine</p>
 
-              </div>
-              </div>
-              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8NHUe0vxBliSKU7znHt7AVQUVuRfYDodfmB4Ju_dNjg0kyg/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+            </div>
           </div>
+        </div>
       </div>
       <div id="content2">
-
-              <div className="container center">
-                  <br/>
-                  <br/>
-                  <h2>Food and Drink!</h2>
-                  <br/>
-                  <br/>
-                  <br/>
-                  <br/>
-              </div>
+        <div className="container center">
+          <div className="col-md-6">
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8NHUe0vxBliSKU7znHt7AVQUVuRfYDodfmB4Ju_dNjg0kyg/viewform?embedded=true" width="100%" height="800px" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
           </div>
+          <div className="col-md-6">
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScx12urkxZGnUCIUL_vtbTJTAynUlsYQNf3OcGVDxqFr-zdNg/viewform?embedded=true" width="100%" height="800px" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+          </div>
+        </div>
+      </div>
+      <div id="secsec">
+        <div className="container">
+          <div id="map-canvas" className="col-md-6"></div>
+          <div className="col-md-4 hidden-xs center" id="location">
+
+              <h2>The Venue</h2>
+              <p>Southport Island, Maine</p>
+
+          </div>
+        </div>
+      </div>
 
       </div>
+
+
     )
   }
 }
