67d103377d78deff8487d3497836691732146683
wjfletcher <warrenjfletcher@gmail.com>
2017-03-23 16:11:38 -0400

    sendgrid setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
-    :authentication => :plain,
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'radiant-atoll-34822.herokuapp.com',
+    :authentication => :plain
   }
-  ActionMailer::Base.delivery_method = :smtp
+
 end
